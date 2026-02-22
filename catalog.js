0474aead3fb3d1cddb56d3aa46160b33dfe585e7
clif1986 <108091102+clif1986@users.noreply.github.com>
2026-02-22 19:23:03 +0200

    Add files via upload

---
+++ catalog.js
@@ -0,0 +1,47 @@
+// База товарів Майстерні Сталі
+const catalogProducts = [
+    {
+        id: "vitr9",
+        category: "Напольні",
+        name: "Вітрило-9",
+        price: 950,
+        old_price: 1450,
+        badge: "Хіт продажів",
+        dimensions: "70 x 18 x 90",
+        bowls: "13 / 18",
+        image: "https://thb.tildacdn.one/tild3337-3565-4162-b833-336436303266/-/empty/image.png"
+    },
+    {
+        id: "kilce12",
+        category: "Напольні",
+        name: "Кільце-12",
+        price: 1045,
+        old_price: 1750,
+        badge: "12 чаш",
+        dimensions: "100 x 18 x 70",
+        bowls: "13 / 18",
+        image: "https://thb.tildacdn.one/tild3432-6636-4362-b836-373763616262/-/empty/IMG_20251001_121014_.jpg" // Заміни на своє фото
+    },
+    {
+        id: "serce3",
+        category: "Настінні",
+        name: "Серце-3",
+        price: 599,
+        old_price: 999,
+        badge: "3 чаші",
+        dimensions: "52 x 21 x 4",
+        bowls: "16 / 21",
+        image: "https://thb.tildacdn.one/tild3964-6132-4539-b730-646434313763/-/empty/image.png" // Заміни на своє фото
+    },
+    {
+        id: "orchidea1",
+        category: "Для орхідей",
+        name: "Стійка для орхідей",
+        price: 850,
+        old_price: 1200,
+        badge: "",
+        dimensions: "40 x 40 x 80",
+        bowls: "10 / 13",
+        image: "https://images.unsplash.com/photo-1497366216548-37526070297c?auto=format&fit=crop&q=80" // Заміни на своє фото
+    }
+];
